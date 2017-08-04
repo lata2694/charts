6a2dd0ee80a40b360682188ebf70274cc3e34100
lata8827 <tiwari.lata786@gmail.com>
2017-08-04 10:14:05 +0530

    moving index.html in app folder from client folder

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ let config = {
     //historyApiFallback : to serve your index.html in place of 404
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src/client/public/index.html'),
+            template: path.join(__dirname, 'index.html'),
             filename: 'index.html',
             multistep: true,
             historyApiFallback: true
